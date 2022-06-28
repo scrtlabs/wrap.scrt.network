50fd1b187d241d66477b2fb1809eff1a56670064
Rurouni <96884945+rurouni0@users.noreply.github.com>
2022-06-28 12:02:00 -0500

    Add global event listener
    add listener for keystorechange

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SecretNetworkClient } from "secretjs";
 import { StyledContent } from "./styled";
 import { Header } from "../Header/Header";
@@ -6,6 +6,7 @@ import { GetPrivacy } from "../GetPrivacy/GetPrivacy";
 import { TokenForm } from "../TokenForm/TokenForm";
 import { Footer } from "../Footer/Footer";
 import { mergeStateType, Token } from "../../types";
+import { notification } from "../../commons";
 
 interface ContentProps {
   mergeState: mergeStateType;
@@ -15,6 +16,20 @@ interface ContentProps {
 export function Content({ mergeState, currentToken }: ContentProps) {
   const [secretjs, setSecretjs] = useState<SecretNetworkClient | null>(null);
   const [secretAddress, setSecretAddress] = useState<string>("");
+
+  useEffect(() => {
+    console.log("test");
+    window.addEventListener("keplr_keystorechange", () => {
+      notification("Keplr Account changed.", "success");
+      setSecretjs(null);
+      setSecretAddress("");
+    });
+
+    return () =>
+      window.removeEventListener("keplr_keystorechange", () => {
+        return;
+      });
+  }, []);
 
   return (
     <StyledContent>
